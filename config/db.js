76205095377e46302134492ac05c24150ae60408
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-05-23 23:56:03 +0800

    Add connection to mongoDB instance

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        // userCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    }); 
+
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+}
+
+module.exports = connectDB;
