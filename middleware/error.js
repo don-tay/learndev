c579ea23ff158eaa59faec312c3594444090463a
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-05-27 00:03:33 +0800

    Add error handling for bad object id request

--- middleware/error.js
+++ middleware/error.js
@@ -1,10 +1,22 @@
+const ErrorResponse = require('../utils/ErrorResponse');
+
 const errorHandler = (err, req, res, next) => {
     // Log to console for dev
     console.log(err.stack.red);
+    console.error(err.name.bgRed);
 
-    res.status(500).json({
+    let error = { ...err };
+    error.message = err.message;
+
+    // Mongoose bad ObjectId
+    if (err.name === 'CastError') {
+        const message = `Resource not found with id of ${err.value}`;
+        error = new ErrorResponse(message, 404);
+    }
+
+    res.status(error.statusCode || 500).json({
         success: false,
-        error: err.message
+        error: error.message || `Server Error`
     });
 };
 
