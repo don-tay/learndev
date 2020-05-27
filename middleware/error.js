54b65b1e3c98b697a7c8db1d6c2153d23819acd9
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-05-27 23:37:40 +0800

    Add error handling for input validation duplicate and missing fields

--- middleware/error.js
+++ middleware/error.js
@@ -3,7 +3,9 @@ const ErrorResponse = require('../utils/ErrorResponse');
 const errorHandler = (err, req, res, next) => {
     // Log to console for dev
     console.log(err.stack.red);
-    console.error(err.name.bgRed);
+    console.error(`err.name is ${err.name}`.bgRed);
+    console.error(`err.code is ${err.code}`.bgRed);
+    // console.error(err);
 
     let error = { ...err };
     error.message = err.message;
@@ -12,6 +14,18 @@ const errorHandler = (err, req, res, next) => {
     if (err.name === 'CastError') {
         const message = `Resource not found with id of ${err.value}`;
         error = new ErrorResponse(message, 404);
+    }
+
+    // Mongoose input validation error
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(e => e.message);
+        error = new ErrorResponse(message, 400);
+    }
+
+    // Mongoose duplicate key
+    if (err.code === 11000) {
+        const message = 'Duplicate field value entered'
+        error = new ErrorResponse(message, 400);
     }
 
     res.status(error.statusCode || 500).json({
