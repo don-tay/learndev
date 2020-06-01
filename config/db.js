a06e5490aba9db40630a22ad73f113dfcf394c93
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-06-01 14:14:31 +0800

    Add course model and seeder to load course files

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
-        userCreateIndex: true,
+        useCreateIndex: true,
         useFindAndModify: false,
         useUnifiedTopology: true
     }); 
