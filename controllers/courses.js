d714cbf862092575c8f31a9250c481f6a7dc7aa2
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-06-01 17:28:47 +0800

    Add population of bootcamp name and description when getting courses

--- controllers/courses.js
+++ controllers/courses.js
@@ -8,13 +8,16 @@ const asyncHandler = require('../middleware/async');
 // @access   Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
     let query;
-    console.log(req.params);
+
     if (req.params.bootcampId) {
         query = Course.find({ bootcamp: req.params.bootcampId });
     } else {
-        query = Course.find();
+        query = Course.find().populate({
+            path: 'bootcamp',
+            select: 'name description'
+        });
     }
-    console.log(query);
+
     const courses = await query;
 
     res.status(200).json({
