6bd5408fb7be8a1b89da240c2dfb2e9799fb22c3
Don Tay Xi Cheng <don.tay@shopback.com>
2020-12-01 17:18:28 +0800

    Update dependencies

--- controllers/courses.js
+++ controllers/courses.js
@@ -8,113 +8,141 @@ const asyncHandler = require('../middleware/async');
 // @route   GET /api/v1/bootcamps/:bootcampId/courses
 // @access  Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
-    if (req.params.bootcampId) {
-        const courses = await Course.find({ bootcamp: req.params.bootcampId });
-        
-        return res.status(200).json({
-            success: true,
-            count: courses.length,
-            data: courses
-        });
-        
-    } else {
-        res.status(200).json(res.advancedResults);
-    }
+  if (req.params.bootcampId) {
+    const courses = await Course.find({ bootcamp: req.params.bootcampId });
+
+    return res.status(200).json({
+      success: true,
+      count: courses.length,
+      data: courses
+    });
+  } else {
+    res.status(200).json(res.advancedResults);
+  }
 });
 
 // @desc    Get single course
 // @route   GET /api/v1/courses/:id
 // @access  Public
 exports.getCourse = asyncHandler(async (req, res, next) => {
-    const { id } = req.params;
-    const query = Course.findById(id).populate({
-        path: 'bootcamp',
-        select: 'name description'
-    });
+  const { id } = req.params;
+  const query = Course.findById(id).populate({
+    path: 'bootcamp',
+    select: 'name description'
+  });
 
-    const course = await query;
+  const course = await query;
 
-    if (!course) {
-        return next(new ErrorResponse(`No course with the id of ${req.params.id}`, 404));
-    }
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`, 404)
+    );
+  }
 
-    res.status(200).json({
-        success: true,
-        data: course
-    });
+  res.status(200).json({
+    success: true,
+    data: course
+  });
 });
 
 // @desc    Add single course
 // @route   POST /api/v1/bootcamps/:bootcampId/courses
 // @access  Private
 exports.addCourse = asyncHandler(async (req, res, next) => {
-    req.body.bootcamp = req.params.bootcampId;
-    req.body.user = req.user.id;
+  req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user.id;
 
-    const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 
-    if (!bootcamp) { // if bootcamp id not found, throw error
-        return next(new ErrorResponse(`No bootcamp with id: ${req.params.bootcampId} found`, 404));
-    }
+  if (!bootcamp) {
+    // if bootcamp id not found, throw error
+    return next(
+      new ErrorResponse(
+        `No bootcamp with id: ${req.params.bootcampId} found`,
+        404
+      )
+    );
+  }
 
-    // Check user is bootcamp owner
-    if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
-        return next(new ErrorResponse(`User ${req.user.id} is not authorized to add courses to bootcamp ${bootcamp._id}`, 401));
-    }
+  // Check user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to add courses to bootcamp ${bootcamp._id}`,
+        401
+      )
+    );
+  }
 
-    const course = await Course.create(req.body);
+  const course = await Course.create(req.body);
 
-    res.status(200).json({
-        success: true,
-        data: course
-    });
+  res.status(200).json({
+    success: true,
+    data: course
+  });
 });
 
 // @desc    Update single course
 // @route   PUT /api/v1/courses/:id
 // @access  Private
 exports.updateCourse = asyncHandler(async (req, res, next) => {
-    let course = await Course.findById(req.params.id);
+  let course = await Course.findById(req.params.id);
 
-    if (!course) { // if bootcamp id not found, throw error
-        return next(new ErrorResponse(`No course with id: ${req.params.id} found`, 404));
-    }
+  if (!course) {
+    // if bootcamp id not found, throw error
+    return next(
+      new ErrorResponse(`No course with id: ${req.params.id} found`, 404)
+    );
+  }
 
-    // Check user is course owner
-    if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
-        return next(new ErrorResponse(`User ${req.user.id} is not authorized to update course ${course._id}`, 401));
-    }
+  // Check user is course owner
+  if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to update course ${course._id}`,
+        401
+      )
+    );
+  }
 
-    course = await Course.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true
-    });
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-    res.status(200).json({
-        success: true,
-        data: course
-    });
+  res.status(200).json({
+    success: true,
+    data: course
+  });
 });
 
 // @desc    Delete single course
 // @route   DELETE /api/v1/courses/:id
 // @access  Private
 exports.deleteCourse = asyncHandler(async (req, res, next) => {
-    const course = await Course.findById(req.params.id);
+  const course = await Course.findById(req.params.id);
 
-    if (!course) { // if bootcamp id not found, throw error
-        return next(new ErrorResponse(`No course with id: ${req.params.id} found`, 404));
-    }
-    
-    // Check user is course owner
-    if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
-        return next(new ErrorResponse(`User ${req.user.id} is not authorized to delete course ${course._id}`, 401));
-    }
+  if (!course) {
+    // if bootcamp id not found, throw error
+    return next(
+      new ErrorResponse(`No course with id: ${req.params.id} found`, 404)
+    );
+  }
 
-    await course.remove();
+  // Check user is course owner
+  if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to delete course ${course._id}`,
+        401
+      )
+    );
+  }
 
-    res.status(200).json({
-        success: true,
-        data: {}
-    });
+  await course.remove();
+
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
 });
--- middleware/advancedResults.js
+++ middleware/advancedResults.js
@@ -1,86 +1,86 @@
-const advancedResults = (model, populate) => async (req,res, next) => {
-    let query;
-    
-    // Copy req.query, if any
-    const reqQuery = { ...req.query };
+const advancedResults = (model, populate) => async (req, res, next) => {
+  let query;
 
-    // Query fields to exclude from reqQuery
-    const removeFields = ['select', 'sort', 'page', 'limit'];
+  // Copy req.query, if any
+  const reqQuery = { ...req.query };
 
-    // Remove fields from reqQuery
-    removeFields.forEach(field => delete reqQuery[field]);
+  // Query fields to exclude from reqQuery
+  const removeFields = ['select', 'sort', 'page', 'limit'];
 
-    // Create query string
-    let queryStr = JSON.stringify(reqQuery);
+  // Remove fields from reqQuery
+  removeFields.forEach(field => delete reqQuery[field]);
 
-    // format into str for mongoDB op (ie. prepend with '$')
-    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
-    
-    // console.log(queryStr);
+  // Create query string
+  let queryStr = JSON.stringify(reqQuery);
 
-    // Find query in db
-    query = model.find(JSON.parse(queryStr));
+  // format into str for mongoDB op (ie. prepend with '$')
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 
-    // Handle select field queries (NOTE: select queries delimited by ',' with no whitespace. eg. ?select=name,description)
-    if (req.query.select) {
-        // split string value in select key into array delimited by ',' then join array back into single string delimited by whitespace
-        const fields = req.query.select.split(',').join(' ');
-        query = query.select(fields);
-    }
+  // console.log(queryStr);
 
-    // Handle sort
-    if (req.query.sort) {
-        const sortBy = req.query.sort.split(',').join(' ');
-        query = query.sort(sortBy);
-    } else { 
-        // Default sort by createdAt date (in descending order, most recent first)
-        query = query.sort('-createdAt');
-    }
+  // Find query in db
+  query = model.find(JSON.parse(queryStr));
 
-    // Pagination
-    const page = parseInt(req.query.page, 10) || 1;
-    const limit = parseInt(req.query.limit,10) || 25;
-    
-    // start and end entry number for a page
-    const startIndex = (page - 1) * limit; 
-    const endIndex = page * limit;
+  // Handle select field queries (NOTE: select queries delimited by ',' with no whitespace. eg. ?select=name,description)
+  if (req.query.select) {
+    // split string value in select key into array delimited by ',' then join array back into single string delimited by whitespace
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
 
-    const totalEntries = await model.countDocuments();
+  // Handle sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    // Default sort by createdAt date (in descending order, most recent first)
+    query = query.sort('-createdAt');
+  }
 
-    query = query.skip(startIndex).limit(limit);
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
 
-    // Populate each model with corresponsing populate model (using virtuals, see Bootcamp model)
-    if (populate) {
-        query = query.populate(populate);
-    }
+  // start and end entry number for a page
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
 
-    const results = await query;
+  const totalEntries = await model.countDocuments();
 
-    // Pagination results
-    const pagination = {};
+  query = query.skip(startIndex).limit(limit);
 
-    if (endIndex < totalEntries) {
-        pagination.next = {
-            page: page + 1,
-            limit
-        }
-    }
+  // Populate each model with corresponsing populate model (using virtuals, see Bootcamp model)
+  if (populate) {
+    query = query.populate(populate);
+  }
 
-    if (startIndex > 0) {
-        pagination.prev ={
-            page: page - 1,
-            limit
-        }
-    }
+  const results = await query;
 
-    res.advancedResults = {
-        success: true,
-        count: results.length,
-        pagination,
-        data: results
-    }
+  // Pagination results
+  const pagination = {};
 
-    next();
+  if (endIndex < totalEntries) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
+
+  res.advancedResults = {
+    success: true,
+    count: results.length,
+    pagination,
+    data: results,
+  };
+
+  next();
 };
 
 module.exports = advancedResults;
